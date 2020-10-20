88013e54dc62a0953d1d3f54a273ffea9bef86c9
Carlos Martinez <carlosmart7104@gmail.com>
2020-10-20 15:41:06 -0500

    Se agregó el código de ejemplo

---
+++ example/index.js
@@ -0,0 +1,5 @@
+// Ejemplo de uso del paquete
+const math = require('../index.js');
+
+console.log('Suma 2 + 2 = ', math.add(2, 2));
+console.log('Multiplicación 2 * 4 = ', math.mult(2, 4));
---
+++ index.js
@@ -0,0 +1,5 @@
+// Módulo de operaciones matemáticas
+module.exports = {
+  add: (a, b) => a + b,
+  mult: (a, b) => a * b
+}
